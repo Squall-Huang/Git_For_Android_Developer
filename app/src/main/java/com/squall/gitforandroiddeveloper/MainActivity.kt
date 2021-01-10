ad06a52a93ebc6b914599ba7c0eddf26d89cafff
Squall Huang <selfi_star@yahoo.com.tw>
2021-01-10 17:23:13 +0800

    final merge change 2

--- app/src/main/java/com/squall/gitforandroiddeveloper/MainActivity.kt
+++ app/src/main/java/com/squall/gitforandroiddeveloper/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("Change 2")
+        println("Change 4")
         println("This line will be merged")
     }
 }
